1d7b69864ef80deb3a5fc430b844f3a6f62a9cdf
Steve Chan <steve2282@hotmail.com>
2022-10-27 14:51:24 +1100

    - change swift list to scrollview, lazyvstack and foreach - hide navigationbartitle and use text element for the title in RecipeListView

--- 19Oct.playground/Contents.swift
+++ 19Oct.playground/Contents.swift
@@ -22,24 +22,24 @@ while (((rand[0] != res[0]) || (rand[1] != res[1]) || (rand[2] != res[2]) || (ra
 */
 
 
-var res = [7, 17, 21, 24, 35, 41]
+var res = [1, 2, 3, 4, 5, 6, 7]
 var rand = [Int]()
 var run2 = 0
 
 repeat {
     repeat {
         rand = []
-        for count in 1...6  {
-            rand.append(Int.random(in: 1...45))
+        for count in 1...7  {
+            rand.append(Int.random(in: 1...35))
         }
         rand.sort()
     }
-    while ((rand[0] == rand[1]) || (rand[1] == rand[2]) || (rand[2] == rand[3]) || (rand[3] == rand[4]) || (rand[4] == rand[5]))
+    while ((rand[0] == rand[1]) || (rand[1] == rand[2]) || (rand[2] == rand[3]) || (rand[3] == rand[4]) || (rand[4] == rand[5]) || (rand[5] == rand[6]))
     run2 += 1
 
-    print("Run: \(run2) \(rand[0]) \(rand[1]) \(rand[2]) \(rand[3]) \(rand[4]) \(rand[5])")
+    print("Run: \(run2) \(rand[0]) \(rand[1]) \(rand[2]) \(rand[3]) \(rand[4]) \(rand[5]) \(rand[6])")
 }
-while (((rand[0] != res[0]) || (rand[1] != res[1]) || (rand[2] != res[2]) || (rand[3] != res[3]) || (rand[4] != res[4]) || (rand[5] != res[5])) && (run2 < 10))
+while (((rand[0] != res[0]) || (rand[1] != res[1]) || (rand[2] != res[2]) || (rand[3] != res[3]) || (rand[4] != res[4]) || (rand[5] != res[5]) || (rand[6] != res[6])) && (run2 < 4))
 
 /*
 var rand = [Int]()
